9f1c50f032b4cc3b977bff9e05c25bd3a51115ab
Rahul Tyagi <rahultyagi915@gmail.com>
2020-07-31 21:22:14 +0530

    css of form corrected

--- src/comps/myForm.js
+++ src/comps/myForm.js
@@ -1,6 +1,31 @@
 import React, { useRef } from "react";
 import { All } from "../constants";
 import useGlobal from "../store";
+const commonStyles = {
+  screenCover: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+};
+const styles = {
+  overlay: {
+    ...commonStyles.screenCover,
+    background: "black",
+    opacity: 0.5,
+  },
+  wrapper: {
+    position: "absolute",
+    top: "3vh",
+    left: 0,
+    right: 0,
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+};
 
 export function MyForm() {
   const colors = All.map((a) => a.color);
@@ -10,28 +35,41 @@ export function MyForm() {
     (s) => s.me.color,
     (a) => a.setSendMe
   );
-  function onSelectClick() {
+  function onSelectClick(ev) {
+    ev.preventDefault();
     const {
       color: { value: color },
       name: { value: name },
     } = ref.current.children;
+    if (!color || !name) {
+      // action show error snack
+      return;
+    }
     setSendMe({ color, name });
   }
 
   return (
     !myColor && (
-      <section ref={ref}>
-        <input type="text" id="name" placeholder="Name" />
-        <select id="color">
-          {colors.map((c) => {
-            if (players.find((p) => p.color === c)) {
-              return null;
-            }
-            return <option value={c}>{c}</option>;
-          })}
-        </select>
-        <button onClick={onSelectClick}>Select</button>
-      </section>
+      <>
+        <div style={styles.overlay}></div>
+        <form onSubmit={onSelectClick} ref={ref} style={styles.wrapper}>
+          <h2>Set your Name and Color to start</h2>
+          <input type="text" id="name" placeholder="Name" />
+          <select id="color">
+            {colors.map((c) => {
+              if (players.find((p) => p.color === c)) {
+                return null;
+              }
+              return (
+                <option key={c} value={c}>
+                  {c}
+                </option>
+              );
+            })}
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
+      </>
     )
   );
 }
